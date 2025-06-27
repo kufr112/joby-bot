2064bab969294e04030fb3a5f8d7078b58ef149e
kufr112 <32547819+kufr112@users.noreply.github.com>
2025-06-27 22:58:29 +0300

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,6 +1,7 @@
+import os
 from supabase import create_client, Client
 
-url = "https://enylqxswgbvfhmhmycia.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVueWxxeHN3Z2J2ZmhtaG15Y2lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzNjI1MDQsImV4cCI6MjA2NTkzODUwNH0.aOQrlXpq5QfQPP-Q6IhWzsMW-pUe5QJcfT_r0xV0WRg"
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-supabase: Client = create_client(url, key)
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
